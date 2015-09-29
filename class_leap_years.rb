ac4f347c7504447724a96897b42a2264c341380d
Tom Dickinson <tom@thomasmdickinson.com>
2015-09-28 21:11:11 -0400

    solve class_leap_years.rb so that in passes all tests

--- class_leap_years.rb
+++ class_leap_years.rb
@@ -4,11 +4,17 @@ require 'rspec'
 #######################################################
 ## YOUR CODE
 def is_a_leap_year?(year)
-  (year % 4 == 0)
+  (year % 400 == 0) || (year % 4 == 0 && year % 100 > 0 )
 end
 
 def leap_years_between(start_year, end_year)
-
+  leap_years = []
+  (start_year..end_year).to_a.each do |year|
+    if is_a_leap_year?(year) == true
+      leap_years << year
+    end
+  end
+  return leap_years
 end
 
 
@@ -32,7 +38,7 @@ RSpec.describe "leap year exercise" do
     end
 
     it "says 1600 is a leap year" do
-      expec(is_a_leap_year?(1600)).to eq true
+      expect(is_a_leap_year?(1600)).to eq true
     end
 
     it "says 1800 is not a leap year" do
